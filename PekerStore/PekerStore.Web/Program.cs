80a2550e2fabcd968891a3ab1fb75acc65ea9dc8
Yasin <yasinpeker43@gmail.com>
2025-09-09 09:01:47 +0300

    feature(program.cs) add ConnectionString

--- PekerStore/PekerStore.Web/Program.cs
+++ PekerStore/PekerStore.Web/Program.cs
@@ -1,6 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using PekerStore.Data.Concrete;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<StoreDbContext>(options =>
+{
+    options.UseMySql(connectionString,ServerVersion.AutoDetect(connectionString));
+});
+
 var app = builder.Build();
+
 app.UseStaticFiles();
 app.MapDefaultControllerRoute();
 app.Run();
