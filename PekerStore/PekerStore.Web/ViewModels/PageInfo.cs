2147b77c4f05716c62374a01c7885c835b1eae3b
Yasin <yasinpeker43@gmail.com>
2025-09-10 09:04:48 +0300

    add taghelper to index

--- PekerStore/PekerStore.Web/ViewModels/PageInfo.cs
+++ PekerStore/PekerStore.Web/ViewModels/PageInfo.cs
@@ -4,6 +4,5 @@ public class PageInfo
 {
     public int TotalItems { get; set; }
     public int ItemsPerPage { get; set; }
-    
     public int TotalPages => (int)Math.Ceiling((decimal)TotalItems / ItemsPerPage);
 }
